c9ca882849d3f71d242e55db0cbea976322e8280
saberzero <saberzero@mynixos.com>
2024-02-28 23:36:49 +0000

    Release #7

--- homeModules/python.nix
+++ homeModules/python.nix
@@ -4,6 +4,14 @@
     home = {
       packages = [
         pkgs.python3
+        pkgs.python311Packages.pandas
+        pkgs.python311Packages.matplotlib
+        pkgs.python311Packages.seaborn
+        pkgs.python311Packages.beautifulsoup4
+        pkgs.python311Packages.pip
+        pkgs.python311Packages.scikitlearn
+        pkgs.python311Packages.numpy
+        pkgs.python311Packages.python-dotenv
         pkgs.python311Packages.black
       ];
     };
