8e62f82c11759244ae427ac29befe51fa9172a9c
saberzero <saberzero@mynixos.com>
2024-03-04 10:10:56 +0000

    Release #7

--- homeModules/nixvim.nix
+++ homeModules/nixvim.nix
@@ -21,7 +21,59 @@
     programs = {
       nixvim = {
         enable = true;
-      };
-    };
-  };
-}
+        clipboard = {
+          register = "unnamedplus";
+        };
+        options = {
+          number = true; # Show line numbers
+          relativenumber = true; # Show relative line numbers
+          shiftwidth = 4; # Tab width should be 2
+        };
+        colorschemes = {
+          tokyonight = {
+            enable = true;
+            style = "storm";
+            styles =
+              {
+                comments =
+                  {
+                    italic = true;
+                  }
+                    floats = "dark";
+                functions =
+                  { }
+                    keywords = {
+                italic = true;
+              }
+                sidebars = "dark";
+            variables = { }
+              }
+              }
+              };
+            plugins = {
+              lightline = {
+                enable = true;
+              }
+                };
+              extraPlugins = with pkgs.vimPlugins; [
+                vim-nix
+              ];
+              globals = {
+                mapleader = " ";
+              };
+              # https://github.com/nix-community/nixvim?tab=readme-ov-file#key-mappings
+              keymaps = [
+                {
+                  mode = "n";
+                  key = ";";
+                  action = ":";
+                }
+              ];
+              extraConfigLua = ''
+                -- Print a little welcome message when nvim is opened!
+                print("Hello world!")
+              '';
+            };
+          };
+        };
+      }
