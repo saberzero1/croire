a20649a9adff67738382a8019c60b9e9a160b764
saberzero <saberzero@mynixos.com>
2024-03-04 09:44:17 +0000

    Release #7

--- homeModules/nixvim.nix
+++ homeModules/nixvim.nix
@@ -1,24 +1,23 @@
 { inputs, ... }@flakeContext:
 { config, lib, pkgs, ... }: {
   config = {
-    home =
+    home = {
       let
-        nixvim = import (builtins.fetchGit {
-          url = "https://github.com/nix-community/nixvim";
-          # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-          # ref = "nixos-23.05";
-        });
+      nixvim = import (builtins.fetchGit {
+        url = "https://github.com/nix-community/nixvim";
+        # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+        # ref = "nixos-23.05";
+      });
       in
-      {
-        packages = [
-          # For home-manager
-          nixvim.homeManagerModules.nixvim
-          # For NixOS
-          # nixvim.nixosModules.nixvim
-          # For nix-darwin
-          # nixvim.nixDarwinModules.nixvim
-        ];
-      };
+      packages = [
+        # For home-manager
+        nixvim.homeManagerModules.nixvim
+        # For NixOS
+        # nixvim.nixosModules.nixvim
+        # For nix-darwin
+        # nixvim.nixDarwinModules.nixvim
+      ];
+    };
     programs = {
       nixvim = {
         enable = true;
