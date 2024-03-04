59587128781aad866a5643520ee8f444447004b0
saberzero <saberzero@mynixos.com>
2024-03-04 11:09:39 +0000

    Release #7

--- homeModules/nixvim.nix
+++ homeModules/nixvim.nix
@@ -16,58 +16,61 @@
         enable = true;
         clipboard = {
           register = "unnamedplus";
-          providers = { }
-            };
-          options = {
-            number = true; # Show line numbers
-            relativenumber = true; # Show relative line numbers
-            shiftwidth = 4; # Tab width should be 2
-          };
-          colorschemes = {
-            tokyonight = {
+          providers =
+            { null
+            }
+              };
+              options = {
+              number = true;         # Show line numbers
+              relativenumber = true; # Show relative line numbers
+              shiftwidth = 4;        # Tab width should be 2
+              };
+              colorschemes = {
+              tokyonight = {
               enable = true;
               style = "storm";
-              styles =
-                {
-                  comments =
-                    {
-                      italic = true;
-                    }
-                      floats = "dark";
-                  functions =
-                    { }
-                      keywords = {
-                  italic = true;
-                }
-                  sidebars = "dark";
-              variables = { }
-                }
-                }
-                };
-              plugins = {
-                lightline = {
-                  enable = true;
-                }
-                  };
-                extraPlugins = with pkgs.vimPlugins; [
-                  vim-nix
-                ];
-                globals = {
-                  mapleader = " ";
-                };
-                # https://github.com/nix-community/nixvim?tab=readme-ov-file#key-mappings
-                keymaps = [
-                  {
-                    mode = "n";
-                    key = ";";
-                    action = ":";
-                  }
-                ];
-                extraConfigLua = ''
-                  -- Print a little welcome message when nvim is opened!
-                  print("Hello world!")
-                '';
+              styles = {
+              comments = {
+              italic = true;
+              }
+              floats = "dark";
+              functions = {
+              null
+              }
+              keywords = {
+              italic = true;
+              }
+              sidebars = "dark";
+              variables = {
+              null
+              }
+              }
+              }
               };
-            };
-          };
-        }
+              plugins = {
+              lightline = {
+              enable = true;
+              }
+              };
+              extraPlugins = with pkgs.vimPlugins; [
+              vim-nix
+              ];
+              globals = {
+              mapleader = " ";
+              };
+              # https://github.com/nix-community/nixvim?tab=readme-ov-file#key-mappings
+              keymaps = [
+              {
+              mode = "n";
+              key = ";";
+              action = ":";
+              }
+              ];
+              extraConfigLua = ''
+      -- Print a little welcome message when nvim is opened!
+      print("Hello world!")
+    '';
+              };
+              };
+              };
+              }
