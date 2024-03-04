bdd07ca959c99ec12ffdbcc43feceb6872a21ba2
saberzero <saberzero@mynixos.com>
2024-03-04 15:48:51 +0000

    Release #7

--- homeModules/nixvim.nix
+++ homeModules/nixvim.nix
@@ -154,9 +154,11 @@
             event = "TextYankPost";
             desc = "Highlight when yanking (copying) text";
             group = "kickstart-highlight-yank";
-            callback = {
-              __raw = "function() vim.highlight.on_yank() end";
-            };
+            callback.__raw = ''
+              function()
+                vim.highlight.on_yank()
+              end
+            '';
           }
         ];
         extraConfigLua = ''
