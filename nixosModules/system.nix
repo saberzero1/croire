04dc5ee5bb449dafc4024d52c7dc2443673d2ef8
saberzero <saberzero@mynixos.com>
2024-03-10 16:36:17 +0000

    Release #7

--- nixosModules/system.nix
+++ nixosModules/system.nix
@@ -7,6 +7,8 @@
     environment = {
       systemPackages = [
         pkgs.libusb
+        pkgs.zsa-udev-rules
+        pkgs.wally-cli
       ];
     };
     fonts = {
@@ -22,6 +24,13 @@
           ];
         };
         enable = true;
+      };
+    };
+    hardware = {
+      keyboard = {
+        zsa = {
+          enable = true;
+        };
       };
     };
     i18n = {
@@ -55,34 +64,6 @@
     services = {
       udev = {
         enable = true;
-        extraRules = ''
-          '''
-          # Rules for Oryx web flashing and live training
-          KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
-          KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
-
-          # Legacy rules for live training over webusb (Not needed for firmware v21+)
-            # Rule for all ZSA keyboards
-            SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="plugdev"
-            # Rule for the Moonlander
-            SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="plugdev"
-            # Rule for the Ergodox EZ
-            SUBSYSTEM=="usb", ATTR{idVendor}=="feed", ATTR{idProduct}=="1307", GROUP="plugdev"
-            # Rule for the Planck EZ
-            SUBSYSTEM=="usb", ATTR{idVendor}=="feed", ATTR{idProduct}=="6060", GROUP="plugdev"
-
-          # Wally Flashing rules for the Ergodox EZ
-          ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
-          ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
-          SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
-          KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
-
-          # Keymapp / Wally Flashing rules for the Moonlander and Planck EZ
-          SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
-          # Keymapp Flashing rules for the Voyager
-          SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
-          '''
-        '';
       };
     };
     system = {
