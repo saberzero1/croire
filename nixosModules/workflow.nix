c7f552d9869b38867590694b3fc129ab08f6ffea
Emile Bangma <ewjbangma@hotmail.com>
2024-03-13 22:14:27 +0100

    Update workflow.nix

--- nixosModules/workflow.nix
+++ nixosModules/workflow.nix
@@ -6,16 +6,15 @@
         pkgs.ulauncher
         pkgs.freerdp
         pkgs.espanso
-        pkgs.appflowy
+        appflowy = pkgs.appflowy.overrideDerivation (oldAttrs: {
+          version = "0.5.1";
+          src = fetchzip {
+            url = "https://github.com/AppFlowy-IO/appflowy/releases/download/0.5.1/AppFlowy-0.5.1-linux-x86_64.tar.gz";
+            hash = "sha256-HET65l+DnSzSu3ZpwJHJ3aRE6WRPW/S/d9aiEy/Onhk=";
+            stripRoot = false;
+          };
+        });
       ];
-      appflowy = pkgs.appflowy.overrideDerivation (oldAttrs: {
-        version = "0.5.1";
-        src = fetchzip {
-          url = "https://github.com/AppFlowy-IO/appflowy/releases/download/0.5.1/AppFlowy-0.5.1-linux-x86_64.tar.gz";
-          hash = "sha256-HET65l+DnSzSu3ZpwJHJ3aRE6WRPW/S/d9aiEy/Onhk=";
-          stripRoot = false;
-        };
-      });
     };
     services = {
       espanso = {
