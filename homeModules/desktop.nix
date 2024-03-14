b3a767491e5e4e6c625c62fa89951b783731a45d
saberzero1 <github@emilebangma.com>
2024-03-14 22:49:29 +0100

    Updated GNOME Dash favorite apps

--- homeModules/desktop.nix
+++ homeModules/desktop.nix
@@ -9,6 +9,9 @@
           picture-uri = "file:///home/saberzero1/Documents/Repos/dotfiles/croire/assets/wallpaper.png";
           picture-uri-dark = "file:///home/saberzero1/Documents/Repos/dotfiles/croire/assets/wallpaper.png";
         };
+        "org/gnome/shell" = {
+          favorite-apps = ["org.gnome.Nautilus.desktop" "Wavebox.desktop" "obsidian.desktop" "discord.desktop" "codium.desktop"];
+        };
       };
     };
   };
