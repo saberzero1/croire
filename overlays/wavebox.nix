d19416a18c8f7ebe606ab2cfd7b1dbe401be6d3d
saberzero1 <github@emilebangma.com>
2024-05-10 17:27:09 +0200

    Wavebox Version Overlay

--- overlays/wavebox.nix
+++ overlays/wavebox.nix
@@ -1,9 +1,9 @@
-self: super {
-  wavebox = super.wavebox.overrideAttrs (old: {
+self: super: {
+  wavebox = super.wavebox.overrideAttrs (oldAttrs: {
     version = "10.124.31-2";
     src = super.fetchurl {
       url = "https://download.wavebox.app/stable/linux/tar/Wavebox_${version}.tar.gz";
       sha256 = "0sws2ndlimhqq73v2205gvgw1zsh7dhjjp1bz8fivxsxwd72n49d";
-    }
-  })
+    };
+  });
 }
