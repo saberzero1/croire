4c29ce467ec7060371c5e90f767371166242aa07
saberzero1 <github@emilebangma.com>
2024-05-10 16:00:28 +0200

    Update scripts

--- updateScripts/update.sh
+++ updateScripts/update.sh
@@ -4,41 +4,66 @@ option="${1}"
 status=$?
 
 case ${option} in
+    flakes)
+        echo "Updating Flakes"
+        $cmd="nix flake update"
+        $cmd
+        $status=$?
+        [ $status -eq 0 ] && echo "Finished updating Flakes" || echo "Updating Flakes failed"
+        ;;
+    clean)
+        echo "Collecting garbage."
+        $cmd="nix-collect-garbage -d"
+        $cmd
+        $status=$?
+        [ $status -eq 0 ] && echo "Finished collecting garbage" || echo "Collecting garbage failed"
+        ;;
     build)
         echo "Building NixOS"
         $cmd="sudo nixos-rebuild build --flake ./croire/. --impure"
+        $cmd
         $status=$?
         [ $status -eq 0 ] && echo "Finished building NixOS" || echo "Building failed"
         ;;
     test)
         echo "Testing NixOS"
         $cmd="sudo nixos-rebuild test --flake ./croire/. --impure"
+        $cmd
         $status=$?
         [ $status -eq 0 ] && echo "Finished testing NixOS" || echo "Testing failed"
         ;;
     update)
         echo "Switching NixOS"
         $cmd="sudo nixos-rebuild switch --flake ./croire/. --impure"
+        $cmd
         $status=$?
         [ $status -eq 0 ] && echo "Finished switching NixOS" || echo "Switching failed"
         ;;
     all)
-        echo "Building, testing, and switching NixOS"
+        echo "Updating Flakes, Building, testing, and switching NixOS"
+        echo "Updating Flakes"
+        $cmd="nix flake update"
+        $cmd
+        $status=$?
+        [ $status -eq 0 ] && echo "Finished updating Flakes" || (echo "Updating Flakes failed" && exit 1)
         echo "Building NixOS"
         $cmd="sudo nixos-rebuild build --flake ./croire/. --impure"
+        $cmd
         $status=$?
         [ $status -eq 0 ] && echo "Finished building NixOS" || (echo "Building failed" && exit 1)
         echo "Testing NixOS"
         $cmd="sudo nixos-rebuild test --flake ./croire/. --impure"
+        $cmd
         $status=$?
         [ $status -eq 0 ] && echo "Finished testing NixOS" || (echo "Testing failed" && exit 1)
         echo "Switching NixOS"
         $cmd="sudo nixos-rebuild switch --flake ./croire/. --impure"
+        $cmd
         $status=$?
         [ $status -eq 0 ] && echo "Finished switching NixOS" || (echo "Switching failed" && exit 1)
         ;;
     *)
-        echo "`basename ${0}`:usage: [build] | [test] | [update] | [all]"
+        echo "`basename ${0}`:usage: [flakes] | [clean] | [build] | [test] | [update] | [all]"
         exit 1 # Command to come out of the program with status 1
         ;; 
 esac
