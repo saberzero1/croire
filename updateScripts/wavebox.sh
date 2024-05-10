36bc7e7f99eeacf52936ea1e26eff2f5f129e509
saberzero1 <github@emilebangma.com>
2024-05-10 17:06:16 +0200

    Update scripts

--- updateScripts/wavebox.sh
+++ updateScripts/wavebox.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-$version=$(curl "https://download.wavebox.app/stable/linux/latest.json" | jq --raw-output '.["urls"]["tar"] | match("https://download.wavebox.app/stable/linux/tar/Wavebox_(.+).tar.gz").captures[0]["string"]')
-echo "${version}"
+echo $(curl "https://download.wavebox.app/stable/linux/latest.json" | jq --raw-output '.["urls"]["tar"] | match("https://download.wavebox.app/stable/linux/tar/Wavebox_(.+).tar.gz").captures[0]["string"]')
+#$version=$(curl "https://download.wavebox.app/stable/linux/latest.json" | jq --raw-output '.["urls"]["tar"] | match("https://download.wavebox.app/stable/linux/tar/Wavebox_(.+).tar.gz").captures[0]["string"]')
 #nix-update wavebox --version "$version"
