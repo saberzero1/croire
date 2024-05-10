225930757c4dbcbe99534c946ddb9baedd5a7363
saberzero1 <github@emilebangma.com>
2024-05-10 16:20:52 +0200

    Update scripts

--- nixos.sh
+++ nixos.sh
@@ -6,54 +6,46 @@ status=$?
 case ${option} in
     flakes)
         echo "Updating Flakes"
-        $cmd="nix flake update"
-        $cmd
+        nix flake update
         $status=$?
         [ $status -eq 0 ] && echo "Finished updating Flakes" || echo "Updating Flakes failed"
         ;;
     clean)
         echo "Collecting garbage."
-        $cmd="nix-collect-garbage -d"
-        $cmd
+        nix-collect-garbage -d
         $status=$?
         [ $status -eq 0 ] && echo "Finished collecting garbage" || echo "Collecting garbage failed"
         ;;
     build)
         echo "Building NixOS"
-        $cmd="sudo nixos-rebuild build --flake . --impure"
-        $cmd
+        sudo nixos-rebuild build --flake . --impure
         $status=$?
         [ $status -eq 0 ] && echo "Finished building NixOS" || echo "Building failed"
         ;;
     test)
         echo "Testing NixOS"
-        $cmd="sudo nixos-rebuild test --flake . --impure"
-        $cmd
+        sudo nixos-rebuild test --flake . --impure
         $status=$?
         [ $status -eq 0 ] && echo "Finished testing NixOS" || echo "Testing failed"
         ;;
     update)
         echo "Switching NixOS"
-        $cmd="sudo nixos-rebuild switch --flake . --impure"
-        $cmd
+        sudo nixos-rebuild switch --flake . --impure
         $status=$?
         [ $status -eq 0 ] && echo "Finished switching NixOS" || echo "Switching failed"
         ;;
     all)
         echo "Building, testing, and switching NixOS"
         echo "Building NixOS"
-        $cmd="sudo nixos-rebuild build --flake . --impure"
-        $cmd
+        sudo nixos-rebuild build --flake . --impure
         $status=$?
         [ $status -eq 0 ] && echo "Finished building NixOS" || (echo "Building failed" && exit 1)
         echo "Testing NixOS"
-        $cmd="sudo nixos-rebuild test --flake . --impure"
-        $cmd
+        sudo nixos-rebuild test --flake . --impure
         $status=$?
         [ $status -eq 0 ] && echo "Finished testing NixOS" || (echo "Testing failed" && exit 1)
         echo "Switching NixOS"
-        $cmd="sudo nixos-rebuild switch --flake . --impure"
-        $cmd
+        sudo nixos-rebuild switch --flake . --impure
         $status=$?
         [ $status -eq 0 ] && echo "Finished switching NixOS" || (echo "Switching failed" && exit 1)
         ;;
