cb1e7dd383f20af008b87bd005205598d63863eb
saberzero1 <github@emilebangma.com>
2024-07-19 22:52:29 +0200

    Updated Wavebox

--- overlays/wavebox.nix
+++ overlays/wavebox.nix
@@ -1,9 +1,9 @@
 self: super: {
   wavebox = super.wavebox.overrideAttrs (oldAttrs: {
-    version = "10.124.32-2";
+    version = "10.126.22-2";
     src = super.fetchurl {
-      url = "https://download.wavebox.app/stable/linux/tar/Wavebox_10.124.32-2.tar.gz";
-      sha256 = "sha256-m6hu7lbHA3I2lIhAfSXF6Vepa+V6h33R1gA6z3eDUrg=";
+      url = "https://download.wavebox.app/stable/linux/tar/Wavebox_10.126.22-2.tar.gz";
+      sha256 = "sha256-geAyd6R0w5pc7ERFPhHTPc4xj0n9VPPEn3ftmD7RXhg=";
     };
   });
 }
