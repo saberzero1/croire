b9703c07fd3e2a955174b0fc9d05c2f3429910e7
saberzero1 <github@emilebangma.com>
2024-10-24 17:51:11 +0200

    Hyprland test

--- nixosConfigurations/croire-low.nix
+++ nixosConfigurations/croire-low.nix
@@ -27,10 +27,14 @@ let
       inputs.hyprland.nixosModules.default
       {
         programs.hyprland = {
+          # Install the packages from nixpkgs
           enable = true;
-          xwayland = {
-            enable = true;
-          };
+          # set the flake package
+          package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+          # make sure to also set the portal package, so that they are in sync
+          portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+          # Whether to enable XWayland
+          xwayland.enable = true;
         };
       }
     ];
