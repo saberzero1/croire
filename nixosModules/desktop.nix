fa78331462271f63b6ba6cac64e5a171fb247663
saberzero1 <github@emilebangma.com>
2024-10-24 19:18:39 +0200

    i3 test

--- nixosModules/desktop.nix
+++ nixosModules/desktop.nix
@@ -54,6 +54,10 @@ in
         pkgs.nautilus-open-any-terminal
         pkgs.xrdp
         pkgs.freerdp
+        pkgs.i3
+        pkgs.i3status
+        pkgs.nitrogen
+        pkgs.rofi
       ];
       # Most of these are optional programs added by services.gnome.core-services
       # and etc., but the module sets other useful options so it is better to
@@ -123,6 +127,14 @@ in
         displayManager = {
           gdm = {
             enable = true;
+          };
+        };
+        windowManager = {
+          i3 = {
+            enable = true;
+            extraPackages = with pkgs; [
+              i3status
+            ];
           };
         };
       };
