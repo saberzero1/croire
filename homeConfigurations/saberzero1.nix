9a15d7210fc8783f7eda599b28f3add51c7aba54
saberzero1 <github@emilebangma.com>
2024-10-24 14:58:45 +0200

    Espanso test

--- homeConfigurations/saberzero1.nix
+++ homeConfigurations/saberzero1.nix
@@ -45,7 +45,7 @@ let
         services = {
           espanso = {
             enable = true;
-            # package = config.programs.espanso.package;
+            package = config.programs.espanso.package;
             serviceConfig = {
               execStart = "${config.programs.espanso.package}/bin/espanso start";
               Restart = "always";
