a91b12a8d1b0578097f113e5d2b6c44cc13d1b73
saberzero1 <github@emilebangma.com>
2024-10-24 21:14:37 +0200

    Restore

--- homeModules/workflow.nix
+++ homeModules/workflow.nix
@@ -57,8 +57,7 @@ in
     # };
     xdg = {
       configFile = {
-        "espanso/config" = { source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Repos/dotfiles-submodules/totten/config"; };
-        "espanso/match" = { source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Repos/dotfiles-submodules/totten/match"; };
+        "espanso" = { source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Repos/dotfiles-submodules/totten"; };
       };
     };
   };
