6c400d8db2ec2fd352bc44cf4fde7f8859d84f7c
saberzero1 <github@emilebangma.com>
2024-10-24 13:48:01 +0200

    Espanso test

--- homeModules/workflow.nix
+++ homeModules/workflow.nix
@@ -42,7 +42,7 @@ in
     #     };
     #   };
     # };
-    systemd = {
+    # systemd = {
       services = {
         espanso = {
     #       enable = true */;
@@ -54,7 +54,7 @@ in
     #       };
         };
       };
-    };
+    # };
     xdg = {
       configFile = {
         "espanso" = { source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Repos/dotfiles-submodules/totten"; };
