0890d8712da8718f9f81d36812b52380ab40e097
saberzero1 <github@emilebangma.com>
2024-10-26 10:46:44 +0200

    ulauncher

--- homeModules/desktop.nix
+++ homeModules/desktop.nix
@@ -69,7 +69,7 @@ in
               Restart = "Always";
               RestartSec = 1;
               ExecStart = pkgs.writeShellScript "ulauncher-env-wrapper.sh" ''
-                export PATH="''${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+                export PATH="${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
                 export GDK_BACKEND=wayland
                 exec ${pkgs.ulauncher}/bin/ulauncher --hide-window
               '';
