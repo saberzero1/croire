8e733892b2261fa32500f6c01b3d7f851d3857bc
saberzero1 <github@emilebangma.com>
2024-10-28 12:44:54 +0100

    Update Wofi

--- nixosModules/desktop.nix
+++ nixosModules/desktop.nix
@@ -56,6 +56,7 @@ in
         pkgs.xrdp
         pkgs.freerdp
         pkgs.albert
+        pkgs.wofi
       ];
       # Most of these are optional programs added by services.gnome.core-services
       # and etc., but the module sets other useful options so it is better to
