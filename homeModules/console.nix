2ed6f0081f001dc5ab4dc20af1458fca415f9202
saberzero1 <github@emilebangma.com>
2024-10-28 14:48:01 +0100

    Auto

--- homeModules/console.nix
+++ homeModules/console.nix
@@ -89,6 +89,7 @@
           eval $(thefuck --alias fuck)
 
           # gnome
+          eval $(dbus-update-activation-environment --systemd --all)
           # eval $(/run/wrappers/bin/gnome-keyring-daemon --start --components=ssh)
         '';
       };
