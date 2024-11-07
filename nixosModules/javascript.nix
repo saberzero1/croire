6643dfb4a198ceeed76e1668b4bf45dbcfc9fd68
saberzero1 <github@emilebangma.com>
2024-11-07 17:02:33 +0100

    Svelte

--- nixosModules/javascript.nix
+++ nixosModules/javascript.nix
@@ -6,6 +6,12 @@
         pkgs.nodePackages.nodejs
         pkgs.nodePackages.typescript
         pkgs.nodePackages.prettier
+        pkgs.nodePackages.eslint_d
+        pkgs.nodePackages.svelte-language-server
+        pkgs.nodePackages.typescript-language-server
+        pkgs.nodePackages.ts-node
+        pkgs.nodePackages.sass
+        pkgs.nodePackages.serve
       ];
     };
     programs = {
