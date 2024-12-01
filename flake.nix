e59aebe7e6a61e5ede874a94500f6ffbd91da51c
saberzero1 <github@emilebangma.com>
2024-12-01 03:59:57 +0100

    flake-utils and python venv

--- flake.nix
+++ flake.nix
@@ -47,36 +47,6 @@
         inherit inputs;
         inherit (self) outputs;
       };
-      envWithScript =
-        script:
-        (pkgs.buildFHSUserEnv {
-          name = "python-env";
-          targetPkgs =
-            pkgs:
-            (with pkgs; [
-              python3
-              python3Packages.pip
-              python3Packages.virtualenv
-              # Support binary wheels from PyPI
-              pythonManylinuxPackages.manylinux2014Package
-              # Enable building from sdists
-              cmake
-              ninja
-              gcc
-              pre-commit
-            ]);
-          runScript = "${
-            pkgs.writeShellScriptBin "runScript" (
-              ''
-                    set -e
-                    test -d .nix-venv || ${pkgs.python3.interpreter} -m venv .nix-venv
-                source .nix-venv/bin/activate
-                set +e
-              ''
-              + script
-            )
-          }/bin/runScript";
-        }).env;
     in
     {
       #overlays = [
@@ -109,7 +79,22 @@
       );
       #legacyPackages.x86_64-linux = pkgs.pkgs;
       legacyPackages.${system} = pkgs.pkgs;
-      devShell = envWithScript "bash";
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python3
+          python3Packages.pip
+          python3Packages.virtualenv
+          pythonManylinuxPackages.manylinux2014Package
+          cmake
+          ninja
+          imagemagick
+        ];
+        shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:${pkgs.pythonManylinuxPackages.manylinux2014Package}/lib:$LD_LIBRARY_PATH";
+          test -d .nix-venv || ${pkgs.python3.interpreter} -m venv .nix-venv
+          source .nix-venv/bin/activate
+        '';
+      };
       username = "saberzero1";
       homeConfigurations = {
         saberzero1 = import ./homeConfigurations/saberzero1.nix flakeContext;
