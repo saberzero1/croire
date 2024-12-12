5f69425379e9fe7a0f7ef8fbd83a72a7409af82a
saberzero1 <github@emilebangma.com>
2024-12-12 13:32:13 +0100

    Espanso Darwin

--- modules/home/darwin/services.nix
+++ modules/home/darwin/services.nix
@@ -1,11 +1,2 @@
-{ pkgs, ... }:
-{
-  services = {
-    espanso = {
-      enable = true;
-      # package = pkgs.espanso;
-      configs = { };
-      matches = { };
-    };
-  };
-}
+{ ... }:
+{ }
