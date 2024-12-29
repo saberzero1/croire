096238477c89879cd0dffe8000578ed7b6efcc58
saberzero1 <github@emilebangma.com>
2024-12-30 00:44:38 +0100

    Homebrew

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -2,6 +2,11 @@
 {
   homebrew = {
     enable = true;
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "none";
+    };
     casks = [
       "ghostty"
       "sol"
