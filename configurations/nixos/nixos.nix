7f0ba0df8dbdcd86822a4d36d86b1bde73ce45bc
saberzero1 <github@emilebangma.com>
2025-03-03 15:29:09 +0100

    Added CleanBrowsing DNS as NixOS default

--- configurations/nixos/nixos.nix
+++ configurations/nixos/nixos.nix
@@ -136,16 +136,49 @@ in
         "nouveau"
       ];
     };
+
+    dnscrypt-proxy2 = {
+      enable = true;
+      # Settings reference:
+      # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
+        # Add this to test if dnscrypt-proxy is actually used to resolve DNS requests
+        # query_log.file = "/var/log/dnscrypt-proxy/query.log";
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+
+        # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+        # server_names = [ ... ];
+        server_names = [
+          "doh-cleanbrowsing-adult"
+          "cleanbrowsing-adult-doh"
+        ];
+      };
+    };
   };
 
   networking = {
+    nameservers = [
+      "127.0.0.1"
+      "::1"
+    ];
     networkmanager = {
       enable = true;
       wifi = {
         backend = "wpa_supplicant";
       };
+      dns = "none";
     };
     useDHCP = false;
+    dhcpcd.extraConfig = "nohook resolv.conf";
     wireless = {
       allowAuxiliaryImperativeNetworks = true;
       dbusControlled = true;
