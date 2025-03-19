5d3e11df413f3d7edbf579a564f0a26f5c5cf902
saberzero1 <github@emilebangma.com>
2025-03-19 22:01:37 +0100

    Caches

--- flake.nix
+++ flake.nix
@@ -7,12 +7,20 @@
     extra-substituters = [
       # nix community's cache server
       "https://nix-community.cachix.org"
+      # cachix's cache server
+      "https://cachix.cachix.org"
+      # nixpkgs cache server
+      "https://nixpkgs.cachix.org"
     ];
 
     # will be appended to the system-level trusted-public-keys
     extra-trusted-public-keys = [
       # nix community's cache server public key
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # cachix's cache server public key
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      # nixpkgs cache server public key
+      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
     ];
   };
 
