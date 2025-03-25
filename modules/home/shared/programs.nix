f94165db7a42598705ac9efc1b2bdf3a6c6b9782
saberzero1 <github@emilebangma.com>
2025-03-25 18:13:03 +0100

    Updated Zed extensions

--- modules/home/shared/programs.nix
+++ modules/home/shared/programs.nix
@@ -211,8 +211,34 @@
         nixd
       ];
       extensions = [
+        "awk"
+        "csharp"
+        "csv"
+        "discord-presence"
+        "docker-compose"
+        "dockerfile"
+        "ejs"
+        "elixir"
+        "env"
+        "erlang"
+        "ini"
+        "just"
+        "libsql-context-server"
+        "live-server"
+        "lua"
+        "make"
         "nix"
+        "python"
+        "python-requirements"
+        "scss"
+        "sql"
+        "ssh-config"
+        "tmux"
         "tokyo-night"
+        "tree-sitter-query"
+        "vscode-icons"
+        "xml"
+        "zig"
       ];
       userKeymaps = pkgs.lib.importJSON "${flake.inputs.dotfiles}/zed/keymap.json";
       userSettings = pkgs.lib.importJSON "${flake.inputs.dotfiles}/zed/settings.json";
