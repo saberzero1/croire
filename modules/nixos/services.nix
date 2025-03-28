82d3f245928548a17e3017915638102109f6ffb9
saberzero1 <github@emilebangma.com>
2025-03-28 16:33:28 +0100

    USB core

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -29,9 +29,9 @@
       ];
       # Disable autosuspend on all USB keyboards and mice
       extraRules = ''
-        ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="on"
-        ACTION=="add", SUBSYSTEM=="usb", TEST=="power/autosuspend", ATTR{power/autosuspend}="0"
-        ACTION=="add", SUBSYSTEM=="usb", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}="0"
+        ACTION=="bind", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="on"
+        ACTION=="bind", SUBSYSTEM=="usb", TEST=="power/autosuspend", ATTR{power/autosuspend}="0"
+        ACTION=="bind", SUBSYSTEM=="usb", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}="0"
       '';
     };
 
