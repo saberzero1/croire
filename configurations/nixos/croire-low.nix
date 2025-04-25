2e0f09788f1159f18c572410b3d7df6caa8c82b2
saberzero1 <github@emilebangma.com>
2025-04-25 20:11:22 +0200

    croire-low

---
+++ configurations/nixos/croire-low.nix
@@ -0,0 +1,207 @@
+# This is your nixos configuration.
+# For home configuration, see /modules/home/*
+{
+  flake,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  imports = [
+    self.nixosModules.default
+    self.nixosModules.fonts
+    self.nixosModules.security
+    self.nixosModules.services
+    self.nixosModules.system
+    inputs.nix-snapd.nixosModules.default
+    {
+      services.snap.enable = true;
+    }
+  ];
+
+  # The platform the configuration will be used on.
+  nixpkgs = {
+    hostPlatform.system = "x86_64-linux";
+    config = {
+      allowUnfree = true;
+      allowBroken = true;
+      nvidia.acceptLicense = true;
+    };
+    overlays = lib.attrValues self.overlays;
+  };
+
+  boot = {
+    extraModulePackages = [ ];
+    hardwareScan = true;
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "vmd"
+        "nvme"
+        "usb_storage"
+        "sd_mod"
+      ];
+      kernelModules = [ ];
+    };
+    kernelModules = [
+      "kvm-intel"
+    ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      systemd-boot = {
+        enable = true;
+      };
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/9016f5b3-7a10-431b-9e6b-88d72d0ceee4";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/1F95-4581";
+      fsType = "vfat";
+    };
+  };
+
+  hardware = {
+    enableRedistributableFirmware = true;
+    pulseaudio = {
+      enable = false;
+    };
+    # https://discourse.nixos.org/t/issue-after-sound-option-was-removed-in-unstable/49394/8
+    alsa = {
+      enablePersistence = true;
+    };
+  };
+
+  networking = {
+    hostName = "croire-low";
+    networkmanager = {
+      enable = true;
+      wifi = {
+        backend = "wpa_supplicant";
+      };
+    };
+    useDHCP = false;
+    wireless = {
+      allowAuxiliaryImperativeNetworks = true;
+      dbusControlled = true;
+      userControlled = {
+        enable = true;
+        group = "networkmanager";
+      };
+    };
+  };
+
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+
+  security = {
+    rtkit = {
+      enable = true;
+    };
+  };
+
+  services = {
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse = {
+        enable = true;
+      };
+      jack = {
+        enable = true;
+      };
+      audio = {
+        enable = true;
+      };
+    };
+    printing = {
+      enable = true;
+    };
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = "saberzero1";
+      };
+    };
+    xserver = {
+      displayManager = {
+        gdm = {
+          enable = true;
+        };
+      };
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+  };
+
+  swapDevices = [
+    {
+      device = "/dev/disk/by-uuid/508b0146-f5ee-4b57-9a1a-c76809ca5597";
+    }
+  ];
+
+  system = {
+    stateVersion = "24.11";
+  };
+
+  systemd = {
+    services = {
+      "autovt@tty1" = {
+        enable = false;
+      };
+      "getty@tty1" = {
+        enable = false;
+      };
+    };
+  };
+
+  users = {
+    users = {
+      saberzero1 = {
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+          "plugdev"
+          "input"
+        ];
+        name = "saberzero1";
+      };
+    };
+  };
+
+  virtualisation = {
+    docker = {
+      # Don't enable docker on low-spec hardware.
+      enable = false;
+    };
+  };
+
+  time = {
+    timeZone = lib.mkForce "Europe/Amsterdam";
+  };
+
+}
