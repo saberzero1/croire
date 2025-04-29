738e76d88da55d863ac4a79533f33918061016aa
saberzero1 <github@emilebangma.com>
2025-04-29 22:33:10 +0200

    Configure lock-file outside of home manager

--- modules/home/shared/settings.nix
+++ modules/home/shared/settings.nix
@@ -2,15 +2,16 @@
 {
   home = {
     activation = {
-      lazyVim = lib.hm.dagEntryAfter [ "writeBoundary" ] ''
+      lazyVim = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         echo "Cleaning Neovim plugin cache"
         rm -rf "$HOME/.local/share/nvim/lazy/*"
         rm -rf "$HOME/.cache/nvim/luac/*"
         echo "Cleaning Neovim plugin cache done"
+        echo "Installing LazyVim lockfile"
+        cp -r "$HOME/Repos/shelter/lazy-lock.json" "$HOME/.config/nvim/lazy-lock.json"
+        echo "Installing LazyVim lockfile done"
         echo "Installing Neovim plugins"
-        sudo chmod +w "$HOME/.config/nvim/lazy-lock.json"
-        sudo ${pkgs.neovim}/bin/nvim --headless "+Lazy! restore" "+Lazy! clean" "+qa"
-        sudo chmod -w "$HOME/.config/nvim/lazy-lock.json"
+        ${pkgs.neovim}/bin/nvim --headless "+Lazy! restore" "+Lazy! clean" "+qa"
         echo "Installing Neovim plugins done"
       '';
     };
