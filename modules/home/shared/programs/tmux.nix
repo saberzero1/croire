0ae8428ae71c3c78a6fefdac489d4a6f50b505c7
saberzero1 <github@emilebangma.com>
2025-05-02 10:03:08 +0200

    tmux config

--- modules/home/shared/programs/tmux.nix
+++ modules/home/shared/programs/tmux.nix
@@ -23,36 +23,36 @@
       mode-indicator
       tmux-which-key
     ];
-    extraConfig =
-      ''
-        # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-        set -g default-terminal "xterm-256color"
-        set -ga terminal-overrides ",*256col*:Tc"
-        set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-        set-environment -g COLORTERM "truecolor"
+    extraConfig = ''
+      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
 
-        set-option -g default-shell $SHELL
+      set-option -g default-shell $SHELL
 
-        # Mouse works as expected
-        set-option -g mouse on
+      # Mouse works as expected
+      set-option -g mouse on
 
-        # easy-to-remember split pane commands
-        bind | split-window -h -c "#{pane_current_path}"
-        bind - split-window -v -c "#{pane_current_path}"
-        bind c new-window -c "#{pane_current_path}"
+      # easy-to-remember split pane commands
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
 
-        bind-key -r f run-shell "tmux neww ~/.config/tmux/scripts/tmux-sessionizer"
+      bind-key -r f run-shell "tmux new-window ~/.config/tmux/scripts/tmux-sessionizer"
 
-        # bind-key -r k run-shell "~/.local/scripts/tmux-sessionizer ~/projects/work/tmux-theme"
-      ''
-      + (
-        if pkgs.stdenv.isDarwin then
-          ''
-            set -g default-command $SHELL
-          ''
-        else
-          ''''
-      );
+      # bind-key -r k run-shell "~/.local/scripts/tmux-sessionizer ~/projects/work/tmux-theme"
+
+      set -g default-command $SHELL
+
+      # startup windows
+      tmux new-window -kS -n "scratch" -t 1 zsh
+      tmux new-window -kS -n "editor" -t 2 nvim
+      tmux new-window -kS -n "watcher" -t 3 zsh
+      tmux new-window -kS -n "remote" -t 4 zsh 
+      tmux new-window -kS -n "git" -t 5 lazygit
+    '';
   };
 
   home.packages = [
