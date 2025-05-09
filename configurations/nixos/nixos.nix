cefaf2d3603f596617b8057890d061048c4ac59a
saberzero1 <github@emilebangma.com>
2025-05-09 16:17:45 +0200

    Updated nvidia

--- configurations/nixos/nixos.nix
+++ configurations/nixos/nixos.nix
@@ -102,6 +102,7 @@ in
       modesetting = {
         enable = true;
       };
+
       powerManagement = {
         enable = false;
         finegrained = false;
@@ -110,6 +111,8 @@ in
       nvidiaSettings = true;
 
       prime = {
+        sync.enable = true;
+
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
