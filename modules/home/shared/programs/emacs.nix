04e147452ed92317118ffd00573f4fa419aec055
saberzero1 <github@emilebangma.com>
2025-05-11 13:09:23 +0200

    emacs

--- modules/home/shared/programs/emacs.nix
+++ modules/home/shared/programs/emacs.nix
@@ -1,5 +1,9 @@
 { flake, ... }:
 {
+  imports = [
+    flake.inputs.nix-doom-emacs-unstraightened.hmModule
+  ];
+
   programs.doom-emacs = {
     enable = true;
     doomDir = "${flake.inputs.dotfiles}/doom-emacs";
