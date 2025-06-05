3b91e749824af55b1159f7c9e404261c02b7f2b0
saberzero1 <github@emilebangma.com>
2025-06-05 10:48:25 +0200

    Path

--- modules/home/shared/settings/home/session-path.nix
+++ modules/home/shared/settings/home/session-path.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 {
   home.sessionPath = [
+    "${config.home.homeDirectory}/.config/just"
     "${config.home.homeDirectory}/.config/tmux/scripts"
     "${config.home.homeDirectory}/.local/bin"
   ];
