f0661979bb85cde780f09edafdd89bc3b4cbe2b3
saberzero1 <github@emilebangma.com>
2025-06-11 10:42:31 +0200

    Homebrew

--- modules/home/darwin/settings/home/session-variables.nix
+++ modules/home/darwin/settings/home/session-variables.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ lib, ... }:
+let
+  product = lib.lists.foldr (a: b: a * b) 1;
+in
 {
-  home.sessionVariables = { };
+  home.sessionVariables = {
+    HOMEBREW_AUTO_UPDATE_SEC = product [
+      60
+      60
+      24
+      7
+    ];
+  };
 }
