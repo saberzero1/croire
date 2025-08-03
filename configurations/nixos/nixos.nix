0534c5de627c392508eae8162b225ce3126a4e1c
saberzero1 <github@emilebangma.com>
2025-08-03 21:36:19 +0200

    NVIDIA

--- configurations/nixos/nixos.nix
+++ configurations/nixos/nixos.nix
@@ -116,8 +116,13 @@ in
       nvidiaSettings = true;
 
       prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+          offloadCmdMainProgram = "nvidia-offload";
+        };
         # sync.enable = true;
-        reverseSync.enable = true;
+        # reverseSync.enable = true;
 
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
