e6dfe22ac3d473e143fdd780c5a538a06e8f20a9
Emile Bangma <github@emilebangma.com>
2025-08-10 00:02:21 +0200

    Update nix.nix

--- modules/home/shared/programs/nvf/languages/nix.nix
+++ modules/home/shared/programs/nvf/languages/nix.nix
@@ -3,6 +3,16 @@
   programs.nvf.settings.vim = {
     languages.nix = {
       enable = true;
+      format = {
+        enable = true;
+      };
+      lsp = {
+        enable = true;
+        server = "nixd";
+      };
+      treesitter {
+        enable = true;
+      };
     };
     # servers = {};
   };
