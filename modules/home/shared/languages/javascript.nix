c68f149dfdd0f3edacf6f3b45595b9cf4031d42e
saberzero1 <github@emilebangma.com>
2025-08-26 12:20:46 +0200

    Javascript

--- modules/home/shared/languages/javascript.nix
+++ modules/home/shared/languages/javascript.nix
@@ -1,17 +1,20 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs.nodePackages; [
-    live-server
-    # nodejs
-    typescript
-    prettier
-    eslint_d
-    svelte-language-server
-    typescript-language-server
-    ts-node
-    sass
-    serve
-    npm
-    vscode-langservers-extracted
-  ];
+  home.packages =
+    (with pkgs.nodePackages; [
+      live-server
+      typescript
+      prettier
+      eslint_d
+      svelte-language-server
+      typescript-language-server
+      ts-node
+      sass
+      serve
+      npm
+      vscode-langservers-extracted
+    ])
+    ++ (with pkgs; [
+      bun
+    ]);
 }
