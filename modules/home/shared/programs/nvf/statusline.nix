ce28470c7d452dca272ca89e4748f01f7ef550d5
saberzero1 <github@emilebangma.com>
2025-09-07 21:39:41 +0200

    Statusline

--- modules/home/shared/programs/nvf/statusline.nix
+++ modules/home/shared/programs/nvf/statusline.nix
@@ -19,68 +19,7 @@ in
         lualine_a = lua [
           ''
             {
-              function()
-                -- Copyright (c) 2020-2021 hoob3rt
-                -- MIT license, see LICENSE for more details.
-                local Mode = {}
-
-                -- stylua: ignore
-                Mode.map = {
-                  ['n']      = 'NORMAL   ',
-                  ['no']     = 'O-PENDING',
-                  ['nov']    = 'O-PENDING',
-                  ['noV']    = 'O-PENDING',
-                  ['no\22']  = 'O-PENDING',
-                  ['niI']    = 'NORMAL   ',
-                  ['niR']    = 'NORMAL   ',
-                  ['niV']    = 'NORMAL   ',
-                  ['nt']     = 'NORMAL   ',
-                  ['ntT']    = 'NORMAL   ',
-                  ['v']      = 'VISUAL   ',
-                  ['vs']     = 'VISUAL   ',
-                  ['V']      = 'V-LINE   ',
-                  ['Vs']     = 'V-LINE   ',
-                  ['\22']    = 'V-BLOCK  ',
-                  ['\22s']   = 'V-BLOCK  ',
-                  ['s']      = 'SELECT   ',
-                  ['S']      = 'S-LINE   ',
-                  ['\19']    = 'S-BLOCK  ',
-                  ['i']      = 'INSERT   ',
-                  ['ic']     = 'INSERT   ',
-                  ['ix']     = 'INSERT   ',
-                  ['R']      = 'REPLACE  ',
-                  ['Rc']     = 'REPLACE  ',
-                  ['Rx']     = 'REPLACE  ',
-                  ['Rv']     = 'V-REPLACE',
-                  ['Rvc']    = 'V-REPLACE',
-                  ['Rvx']    = 'V-REPLACE',
-                  ['c']      = 'COMMAND  ',
-                  ['cv']     = 'EX       ',
-                  ['ce']     = 'EX       ',
-                  ['r']      = 'REPLACE  ',
-                  ['rm']     = 'MORE     ',
-                  ['r?']     = 'CONFIRM  ',
-                  ['!']      = 'SHELL    ',
-                  ['t']      = 'TERMINAL ',
-                }
-
-                ---@return string current mode name
-                Mode.get_mode = function()
-                  local hydra_statusline = require("hydra.statusline")
-                  local mode_code = vim.api.nvim_get_mode().mode
-                  if hydra_statusline ~= nil and hydra_statusline.is_active() then
-                    return hydra_statusline.get_name()
-                  end
-                  if Mode.map[mode_code] == nil then
-                    return mode_code
-                  end
-                  return Mode.map[mode_code]
-                end
-
-              	local display_mode = Mode.get_mode()
-
-                return display_mode
-              end,
+              "mode",
               icons_enabled = true,
               separator = {
                 left = "",
