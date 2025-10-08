ab837394791f487aba975843d73a4ed699c92c26
saberzero1 <github@emilebangma.com>
2025-10-08 17:44:20 +0200

    OMNIX

--- configurations/nixos/nixos.nix
+++ configurations/nixos/nixos.nix
@@ -56,7 +56,7 @@ in
     kernelModules = [
       "kvm-intel"
     ];
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_17;
     kernelParams = [
       "usbcore.autosuspend=-1"
     ];
