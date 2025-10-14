7cd8433aacc96bdd9fee40a791c7e7604505cda1
saberzero1 <github@emilebangma.com>
2025-10-14 16:05:38 +0200

    Neovim

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,7 @@ self: super: {
   # ghostty = inputs.ghostty.packages.${self.system}.default;
   # gitbutler = inputs.gitbutler.packages.${self.system}.default;
   neovim = inputs.neovim-nightly-overlay.packages.${self.system}.default;
+  neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${self.system}.default;
   nix-direnv = inputs.nix-direnv.packages.${self.system}.default;
   # nixvim = inputs.akira.packages.${self.system}.default;
   omnix = inputs.omnix.packages.${self.system}.default;
