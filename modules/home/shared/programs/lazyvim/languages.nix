d45c6fa811b0f7dfd8bab79fb0e19100f4b0a4c7
Emile Bangma <github@emilebangma.com>
2025-10-14 07:48:07 +0200

    Update languages.nix

--- modules/home/shared/programs/lazyvim/languages.nix
+++ modules/home/shared/programs/lazyvim/languages.nix
@@ -1,8 +1,15 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  grammarPackages = with pkgs; lib.pipe tree-sitter-grammars.sources [
+  builtins.attrNames
+  (builtins.map (n: tree-sitter-grammars.${n}))
+  (builtins.filter (pkg: !pkg.meta.broken))
+];
+in
 {
   imports = [
     ./languages
   ];
 
-  programs.lazyvim.treesitterParsers = with pkgs.tree-sitter-grammars; builtins.attrValues pkgs.tree-sitter-grammars;
+  programs.lazyvim.treesitterParsers = grammarPackages;
 }
