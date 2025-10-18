ea66f1a5bcc3201774849be7fe9a2221909535a9
saberzero1 <github@emilebangma.com>
2025-10-18 13:30:12 +0200

    Removed Zed

--- modules/home/nixos/programs/zed-editor.nix
+++ modules/home/nixos/programs/zed-editor.nix
@@ -5,8 +5,8 @@ let
 in
 {
   programs.zed-editor = {
-    enable = true;
-    package = pkgs.zed-latest;
+    enable = false;
+    package = pkgs.zed; # pkgs.zed-latest;
     extraPackages = with pkgs; [
       nodejs_latest
       nixd
