6eaaa276bd48ea46674092744dcdc9d33f786fd2
saberzero1 <github@emilebangma.com>
2025-10-19 19:32:29 +0200

    Diff-so-fancy

--- modules/home/darwin/programs/git.nix
+++ modules/home/darwin/programs/git.nix
@@ -11,50 +11,54 @@ in
   # gpg2 --list-secret-keys --keyid-format=long
   # gpg2 --armor --export 1234567890ABCDEF
 
-  programs.git = {
-    diff-so-fancy = {
-      changeHunkIndicators = true;
+  programs = {
+    git = {
       enable = true;
-      markEmptyLines = true;
-      pagerOpts = [
-        "--tabs=4"
-        "-RFX"
+      package = pkgs.gitFull;
+      includes =
+        if isDarwin then
+          [
+            {
+              path = "${config.home.homeDirectory}/work_gitconfig";
+              condition = "gitdir:~/Work/External/Repos/**";
+            }
+          ]
+        else
+          [ ];
+      ignores = [
+        "*.swp"
       ];
-      rulerWidth = null;
-      stripLeadingSymbols = true;
-      useUnicodeRuler = true;
-    };
-    enable = true;
-    package = pkgs.gitFull;
-    userName = "saberzero1";
-    userEmail = "github@emilebangma.com";
-    includes =
-      if isDarwin then
-        [
-          {
-            path = "${config.home.homeDirectory}/work_gitconfig";
-            condition = "gitdir:~/Work/External/Repos/**";
-          }
-        ]
-      else
-        [ ];
-    ignores = [
-      "*.swp"
-    ];
-    lfs = {
-      enable = true;
-    };
-    extraConfig = {
-      init.defaultBranch = "master";
-      core = {
-        editor = "nvim";
-        autocrlf = "input";
+      lfs = {
+        enable = true;
       };
-      commit.gpgsign = true;
-      gpg.format = "ssh";
-      user.signingkey = "${config.home.homeDirectory}/.ssh/saberzero1-github.pub";
-      pull.rebase = true;
-      rebase.autoStash = true;
+      settings = {
+        user = {
+          name = "saberzero1";
+          email = "github@emilebangma.com";
+        };
+        init.defaultBranch = "master";
+        core = {
+          editor = "nvim";
+          autocrlf = "input";
+        };
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        user.signingkey = "${config.home.homeDirectory}/.ssh/saberzero1-github.pub";
+        pull.rebase = true;
+        rebase.autoStash = true;
+      };
     };
+    diff-so-fancy = {
+      enable = true;
+      settings = {
+        changeHunkIndicators = true;
+        markEmptyLines = true;
+        pagerOpts = "--tabs=4 -RFX";
+        stripLeadingSymbols = true;
+        useUnicodeRuler = true;
+      };
+      enableGitIntegration = true;
+    };
+
   };
 }
--- modules/home/nixos/programs/git.nix
+++ modules/home/nixos/programs/git.nix
@@ -1,50 +1,54 @@
 { pkgs, config, ... }:
 {
-  programs.git = {
-    signing = {
-      key = null;
-      signByDefault = true;
+  programs = {
+    git = {
+      signing = {
+        key = null;
+        signByDefault = true;
+      };
+      enable = true;
+      package = pkgs.gitFull;
+      ignores = [
+        "*.swp"
+      ];
+      lfs = {
+        enable = true;
+      };
+      /*
+        signing = {
+        # key = null;
+        signByDefault = true;
+        gpgPath = "${pkgs.gnupg}/bin/gpg2";
+        };
+      */
+      settings = {
+        user = {
+          name = "saberzero1";
+          email = "github@emilebangma.com";
+        };
+        init.defaultBranch = "master";
+        core = {
+          editor = "nvim";
+          autocrlf = "input";
+        };
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        user.signingkey = "${config.home.homeDirectory}/.ssh/saberzero1-github.pub";
+        pull.rebase = true;
+        rebase.autoStash = true;
+      };
     };
     diff-so-fancy = {
-      changeHunkIndicators = true;
       enable = true;
-      markEmptyLines = true;
-      pagerOpts = [
-        "--tabs=4"
-        "-RFX"
-      ];
-      rulerWidth = null;
-      stripLeadingSymbols = true;
-      useUnicodeRuler = true;
-    };
-    enable = true;
-    package = pkgs.gitFull;
-    ignores = [
-      "*.swp"
-    ];
-    lfs = {
-      enable = true;
-    };
-    /*
-      signing = {
-      # key = null;
-      signByDefault = true;
-      gpgPath = "${pkgs.gnupg}/bin/gpg2";
+      settings = {
+        changeHunkIndicators = true;
+        markEmptyLines = true;
+        pagerOpts = "--tabs=4 -RFX";
+        stripLeadingSymbols = true;
+        useUnicodeRuler = true;
       };
-    */
-    userName = "saberzero1";
-    userEmail = "github@emilebangma.com";
-    extraConfig = {
-      init.defaultBranch = "master";
-      core = {
-        editor = "nvim";
-        autocrlf = "input";
-      };
-      commit.gpgsign = true;
-      gpg.format = "ssh";
-      user.signingkey = "${config.home.homeDirectory}/.ssh/saberzero1-github.pub";
-      pull.rebase = true;
-      rebase.autoStash = true;
+      enableGitIntegration = true;
     };
+
   };
 }
