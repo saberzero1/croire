f984d56355030f6207a90c8dbeef2b083182a631
Emile Bangma <github@emilebangma.com>
2025-10-19 15:21:20 +0200

    Update emacs.nix

--- modules/home/shared/programs/emacs.nix
+++ modules/home/shared/programs/emacs.nix
@@ -7,10 +7,11 @@ in
   imports = [
     flake.inputs.nix-doom-emacs-unstraightened.hmModule
   ];
+  # https://github.com/marienz/nix-doom-emacs-unstraightened/blob/main/home-manager.nix
   programs.doom-emacs = {
     enable = true;
     doomDir = "${self}/programs/doom-emacs";
     doomLocalDir = "${config.home.homeDirectory}/doom.d";
-    provideEmacs = true;
+    provideEmacs = false;
   };
 }
