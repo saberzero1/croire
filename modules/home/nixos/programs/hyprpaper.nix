bc67f7a9474a09cd00aff6b2dc53002f315fbf8b
saberzero1 <github@emilebangma.com>
2025-10-24 00:06:32 +0200

    Set wallpaper

--- modules/home/nixos/programs/hyprpaper.nix
+++ modules/home/nixos/programs/hyprpaper.nix
@@ -8,10 +8,10 @@ in
     enable = true;
     settings = {
       preload = [
-        "${self}/assets/backgrounds/wallpaper_night.png"
+        "${self}/assets/backgrounds/wallpaper_pixel_neon.png"
       ];
       wallpaper = [
-        ",${self}/assets/backgrounds/wallpaper_night.png"
+        "eDP-1,${self}/assets/backgrounds/wallpaper_pixel_neon.png"
       ];
       splash = false;
       ipc = "on";
