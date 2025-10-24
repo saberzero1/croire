18e9294d25240acd5a704f582134d5fc14482d05
saberzero1 <github@emilebangma.com>
2025-10-24 15:51:46 +0200

    LazyVim treesitter

--- modules/home/shared/programs/lazyvim/languages.nix
+++ modules/home/shared/programs/lazyvim/languages.nix
@@ -1,15 +1,25 @@
 { lib, pkgs, ... }:
 let
-  grammarPackages = builtins.attrValues pkgs.vimPlugins.nvim-treesitter.grammarPlugins;
+  grammarPlugins = builtins.attrValues pkgs.vimPlugins.nvim-treesitter.grammarPlugins;
+  grammarPackages = builtins.attrValues pkgs.tree-sitter-grammars;
   filterNonPackage = builtins.filter lib.isDerivation;
   filterBroken = builtins.filter (n: !n.meta.broken);
   filterEmpty = builtins.filter (n: n.pname or "" != "");
-  allGrammars = filterEmpty (filterBroken (filterNonPackage grammarPackages));
+  allGrammarPlugins = filterEmpty (filterBroken (filterNonPackage grammarPlugins));
+  allGrammarPackages = filterEmpty (filterBroken (filterNonPackage grammarPackages));
 in
 {
   imports = [
     ./languages
   ];
+
+  programs.lazyvim.extraPackages =
+    with pkgs;
+    [
+      tree-sitter
+    ]
+    ++ allGrammarPackages
+    ++ allGrammarPlugins;
 
   # programs.lazyvim.treesitterParsers = allGrammars;
 }
