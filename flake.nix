892d50c27409818ae8b464f6c2d1e359ad17a04e
saberzero1 <github@emilebangma.com>
2025-10-28 10:19:45 +0100

    Flakehub

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
     # Unified Nix sources
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+    # nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
     # nixpkgs-stable.url = "github:NixOS/nixpkgs";
     sops-nix.url = "https://flakehub.com/f/Mic92/sops-nix/0.1";
     # sops-nix.url = "github:Mic92/sops-nix";
@@ -113,8 +113,8 @@
 
     # Home-manager
     home-manager = {
-      url = "https://flakehub.com/f/nix-community/home-manager/*";
-      # url = "github:nix-community/home-manager";
+      # url = "https://flakehub.com/f/nix-community/home-manager/*";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
