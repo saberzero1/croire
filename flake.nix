93e1696de6888341b41caaf53841cd2143803939
saberzero1 <github@emilebangma.com>
2025-10-30 07:39:37 +0100

    Flake Parts

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
     nix-direnv.url = "https://flakehub.com/f/nix-community/nix-direnv/*";
     # nix-direnv.url = "github:nix-community/nix-direnv";
     direnv-instant.url = "github:Mic92/direnv-instant";
-    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1";
+    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/*";
     # flake-parts.url = "github:hercules-ci/flake-parts";
     #nixos-unified.url = "github:srid/nixos-unified";
     nixos-unified.url = "github:saberzero1/nixos-unified/overlays";
