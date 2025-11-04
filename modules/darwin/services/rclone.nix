7a773d90dd7e90b439f73424529ad247e56e97ec
saberzero1 <github@emilebangma.com>
2025-11-04 13:44:13 +0100

    launchd

--- modules/darwin/services/rclone.nix
+++ modules/darwin/services/rclone.nix
@@ -1,20 +1,7 @@
 { pkgs, ... }:
 {
   launchd = {
-    # Fixes "Too many open files" errors
-    daemons."*".serviceConfig = {
-      SoftResourceLimits.NumberOfFiles = 65536;
-      HardResourceLimits.NumberOfFiles = 1048576;
-    };
-    agents."*".serviceConfig = {
-      SoftResourceLimits.NumberOfFiles = 65536;
-      HardResourceLimits.NumberOfFiles = 1048576;
-    };
     user.agents = {
-      "*".serviceConfig = {
-        SoftResourceLimits.NumberOfFiles = 65536;
-        HardResourceLimits.NumberOfFiles = 1048576;
-      };
       rclone-mounts = {
         command = "${pkgs.rclone}/bin/rclone mount hidrive:hidrive ~/hidrive";
         serviceConfig = {
