1070ebf51b2ac237c9b3e1b1eed24cd151587c28
saberzero1 <github@emilebangma.com>
2026-01-30 10:00:11 +0100

    fix(nu): tmux-sessionizer

--- modules/home/shared/programs/tmux-sessionizer.nix
+++ modules/home/shared/programs/tmux-sessionizer.nix
@@ -15,20 +15,20 @@
     ];
     maxDepth = 3;
     sessionCommands = [
-      "tmux renamew -t 1 'scratch' && $SHELL"
-      "tmux renamew -t 2 'editor' && nvim"
-      "tmux renamew -t 3 'watcher' && $SHELL"
-      "tmux renamew -t 4 'agent' && opencode"
-      "tmux renamew -t 5 'git' && lazygit"
+      "tmux renamew -t 1 'scratch'; $SHELL"
+      "tmux renamew -t 2 'editor'; nvim"
+      "tmux renamew -t 3 'watcher'; $SHELL"
+      "tmux renamew -t 4 'agent'; opencode"
+      "tmux renamew -t 5 'git'; lazygit"
     ];
     sessionTemplate = ''
       #!/usr/bin/env bash
 
-      tmux renamew -t 1 'scratch' && clear
-      tmux neww -kdS -n 'editor' -a -t 2 'nvim' && clear
-      tmux neww -kdS -n 'watcher' -a -t 3 '$SHELL' && clear
-      tmux neww -kdS -n 'remote' -a -t 4 'opencode' && clear
-      tmux neww -kdS -n 'git' -a -t 5 'lazygit' && clear
+      tmux renamew -t 1 'scratch'; clear
+      tmux neww -kdS -n 'editor' -a -t 2 'nvim'
+      tmux neww -kdS -n 'watcher' -a -t 3; clear
+      tmux neww -kdS -n 'remote' -a -t 4 'opencode'
+      tmux neww -kdS -n 'git' -a -t 5 'lazygit'
     '';
   };
 }
