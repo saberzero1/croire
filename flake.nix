566aaedfee2e68ab51b20bea2b67be7c536d7e3a
saberzero1 <github@emilebangma.com>
2026-03-31 12:32:30 +0200

    chore: versioned opencode

--- flake.nix
+++ flake.nix
@@ -195,7 +195,7 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
     opencode = {
-      url = "github:anomalyco/opencode";
+      url = "github:anomalyco/opencode?ref=v1.3.8";
       # inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     opencode-claude-max-proxy = {
